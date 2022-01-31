08149b2bd4412be3888f23b7bc754e2979b29b49
khushali452 <khushalivasani.ict19@gmail.com>
2022-01-31 20:57:56 +0530

    new post component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 
 function App() {
   return (
     <>
     <TopBar />
-    <Single />
+  
+    <Write />
     </>
   );
 }
