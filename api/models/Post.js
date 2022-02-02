d41f9b8abfd2d6efe31ebfeed5e3d6a8b7e6d86c
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-02 20:12:49 +0530

    created models

---
+++ api/models/Post.js
@@ -0,0 +1,32 @@
+const mongoose= require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    desc:{
+        type:String,
+        required:false,
+        
+    },
+    photo:{
+        type:String,
+        required:true,
+        
+    },
+    username:{
+        type:String,
+        required:true,
+    },
+    categories:{
+        type:Array,
+        required:false,
+    },
+
+},
+{timestamps:true}
+);
+
+module.exports=mongoose.model("Post",PostSchema);
