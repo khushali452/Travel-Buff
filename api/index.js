c2d7234d044f2d8ce24fdc3f83b25b60e1c3de25
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-02 20:09:32 +0530

    created models

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
 
-app.use("/",(req,res)=>{
-    console.log("hey this is main url")
-})
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+   
+}).then(console.log("connected to server"))
+.catch((err) => console.log(err));
+
 
 app.listen("5000",() =>{
     console.log("Backend is Running.")
