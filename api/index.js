7ad3414ac761ead7d4b99c94c9dcb73f7d0ff570
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-04 21:02:18 +0530

    login and register routes with nodejs api and connecting login /registeratoin credentials with mongodb

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv=require("dotenv");
 const mongoose=require("mongoose");
+const authRoute =require("./routes/auth");
 
 
 dotenv.config();
+app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
@@ -13,7 +16,9 @@ mongoose.connect(process.env.MONGO_URL,{
 }).then(console.log("connected to server"))
 .catch((err) => console.log(err));
 
+app.use("/api/auth",authRoute);
 
 app.listen("5000",() =>{
     console.log("Backend is Running.")
 });
+
