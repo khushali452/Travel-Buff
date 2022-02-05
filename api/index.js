e213b04536f3ac566acb98d20eec6305da2befbb
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-05 21:22:09 +0530

    user router

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv=require("dotenv");
 const mongoose=require("mongoose");
 const authRoute =require("./routes/auth");
+const userRoute =require("./routes/users");
 
 
 dotenv.config();
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGO_URL,{
 .catch((err) => console.log(err));
 
 app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
 
 app.listen("5000",() =>{
     console.log("Backend is Running.")
