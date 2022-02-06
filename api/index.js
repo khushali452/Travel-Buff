fa36791adfcf18fb4b05dcab6707d5b6806dfabb
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-06 22:48:48 +0530

    uploading images

--- api/index.js
+++ api/index.js
@@ -6,10 +6,13 @@ const authRoute =require("./routes/auth");
 const userRoute =require("./routes/users");
 const postRoute =require("./routes/posts");
 const categoryRoute =require("./routes/categories");
+const multer=require("multer");
 
 
 dotenv.config();
 app.use(express.json());
+
+
 
 
 mongoose.connect(process.env.MONGO_URL,{
@@ -18,6 +21,20 @@ mongoose.connect(process.env.MONGO_URL,{
    
 }).then(console.log("connected to server"))
 .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, "hello.jpg");
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.any(), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
 
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
