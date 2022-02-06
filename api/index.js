0d8fa59285f631f788ccb6d2aea9cafdee187a43
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-06 21:38:13 +0530

    fetch categories

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const mongoose=require("mongoose");
 const authRoute =require("./routes/auth");
 const userRoute =require("./routes/users");
 const postRoute =require("./routes/posts");
+const categoryRoute =require("./routes/categories");
 
 
 dotenv.config();
@@ -21,6 +22,7 @@ mongoose.connect(process.env.MONGO_URL,{
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/posts",postRoute);
+app.use("/api/categories",categoryRoute);
 
 app.listen("5000",() =>{
     console.log("Backend is Running.")
