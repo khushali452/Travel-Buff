00016c85deb67b28c36def9a7cb43a04289ddc01
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-08 21:44:10 +0530

    fetching data from backend

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/header/Header';
 import "./home.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Posts from '../../components/posts/Posts';
+import axios from "axios";
 
 export default function Home() {
+  const [posts,setPosts]=useState([])
+useEffect(()=>{
+  const fetchPosts = async ()=>{
+    const res= await axios.get("posts")
+    console.log(res);
+  }
+  fetchPosts()
+})
+
   return(
     <>
     <Header />
